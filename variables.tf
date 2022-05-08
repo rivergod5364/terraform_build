ee61aa50c7eb0a79d8f6187b55ebc27940ef0f3c
root <root@LAPTOP-ED5V2RUE.localdomain>
2022-05-08 22:43:45 +0900

    firstcommintTeraform

---
+++ variables.tf
@@ -0,0 +1,13 @@
+# ec2
+variable public_key_path {
+  default = "~/.ssh/id_rsa.pub"
+}
+/*
+variable "ami_redhat8" {
+  default = "ami-00c8dfcb0b542ee0c"
+}
+
+variable "ami_redhat8_instance_type" {
+  default = "t2.micro"
+}
+*/
